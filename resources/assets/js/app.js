e8778b34b119ab9c449e11307ff61d34ab7475a4
irvinwatr93 <irvin@watr.mx>
2017-08-28 05:22:39 -0500

    obtener los tweets en componente timeline

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+var Vue = require('vue');
+Vue.use(require('vue-resource'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +17,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('timeline', require('./components/Timeline.vue'));
 
 const app = new Vue({
     el: '#app'
