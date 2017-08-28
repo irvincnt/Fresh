3863872c96d8b71ecbc9b79cecb98f331d08ba02
Irvin Contreras <irvin@watr.mx>
2017-08-28 14:40:20 -0500

    controlador y vista developer

---
+++ app/Http/Controllers/DeveloperController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeveloperController extends Controller
+{
+    public function __constructor()
+    {
+        $this->middleware(['auth']);
+    }
+
+    public function index()
+    {
+
+    }
+}
