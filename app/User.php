7746902aecdb452e339a1690e9ec35307d6f52ed
irvinwatr93 <irvin@watr.mx>
2017-08-28 04:44:34 -0500

    migración, modelo y factory de Tweet

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Tweet;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function tweets() 
+    {
+        return $this->hasMany(Tweet::class);
+    }
 }
